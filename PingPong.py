df5e73659d9473fe5338598f13cbe7dc83dbbf5a
Smick07 <nik2292007@gmail.com>
2024-03-24 20:16:04 +0300

    Добавил ракетки

--- PingPong.py
+++ PingPong.py
@@ -1,11 +1,10 @@
 from pygame import *
-from random import randint 
-from time import time as timer
 
-window = display.set_mode((700,500))
-display.set_caption('Шутер')
-background = transform.scale(image.load('galaxy.jpg'),(700,500))
-window.blit(background,(0,0))
+back = (200,255,255)
+WIDTH = 600
+HEIGHT = 500
+window = display.set_mode((WIDTH, HEIGHT))
+window.fill(back)
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -21,143 +20,45 @@ class GameSprite(sprite.Sprite):
 
 
 class Player(GameSprite):
+    def __init__(self, button_up, button_down, *args):
+        self.button_up = button_up
+        self.button_down = button_down
+        super().__init__(*args)
+
+    
     def update(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_a] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys_pressed[K_d] and self.rect.x < 630:
-            self.rect.x += self.speed
-    def fire(self):
-        global bullets
-        bullet = Bullet('bullet.png', self.rect.centerx, self.rect.top, 15, 20, -15)
-        bullets.add(bullet)
-        fire_sound.play()
-
-
-mixer.init()
-mixer.music.load('Mesmerizing Galaxy Loop.mp3')
-mixer.music.set_volume(0.1)
-mixer.music.play()
-
-fire_sound = mixer.Sound('fire.ogg')
-fire_sound.set_volume(0.1)
-
-rel_time = False
-num_fire = 0
-hp = 100
-lost = 0
-death = 0
-player = Player('rocket.png', 300, 380,80,120, 10)
-
-
-monsters = sprite.Group()
-bullets = sprite.Group()
-asteroids = sprite.Group()
-
-for _ in range(6):
-    monster = Enemy('ufo.png',randint(80,620), -40, 100, 60, randint(2,6))
-    monsters.add(monster)
-for _ in range(3):
-    asteroid = Enemy('asteroid.png',randint(80,620), -40, 100, 60, randint(2,3))
-    asteroids.add(asteroid)
-
-font.init()
-font1 = font.Font(None,36)
-font2 = font.Font(None,60)
-text_lose1 = font2.render('Вы проиграли!', 1 , (255,0,0))
-text_win = font2.render('Вы выиграли!', 1 , (255,0,0))
-text_reload = font1.render('Wait, reload... ', 1, (255 ,0,0))
+        if keys_pressed[self.button_up] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[self.button_down] and self.rect.y < HEIGHT - 155:
+            self.rect.y += self.speed
+   
 
 game = True
 finish = False
+
+
+clock = time.Clock()
+FPS = 60
+
+racket1 = Player(K_w, K_s, 'racket.png',30, 200, 50, 150, 4)
+racket2 = Player(K_UP, K_DOWN, 'racket.png',520, 200, 50, 150, 4)
+
+
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-        elif e.type == KEYDOWN:
-            if e.key == K_SPACE:
-                if num_fire < 5:
-                    player.fire()
-                    num_fire += 1
-                if num_fire >= 5 and rel_time == False:
-                    rel_time = True
-                    start = timer()
-            
+        
     if finish != True:
-        player.update()
-        monsters.update()
-        bullets.update()
-        asteroids.update()
- 
-        collides = sprite.groupcollide(monsters,bullets, True, True)
+        window.fill(back)
+        racket1.update()
+        racket2.update()
         
-        for c in collides:
-            death += 1
-            monster = Enemy('ufo.png',randint(80,620), -40, 100, 60, randint(2,4))
-            monsters.add(monster)
+        racket1.reset()
+        racket2.reset()
 
-        if sprite.spritecollide(player, monsters, True):
-            hp -= 25
-            monster = Enemy('ufo.png',randint(80,620), -40, 100, 60, randint(2,4))
-            monsters.add(monster)
-        if sprite.spritecollide(player, asteroids, True):
-            hp -= 15
-            asteroid = Enemy('asteroid.png',randint(80,620), -40, 100, 60, randint(2,4))
-            asteroids.add(asteroid)
 
-        text_lose = font1.render('Пропущено: ' + str(lost), 1, (255 ,255,255))
-        text_time = font1.render('Убито: ' + str(death), 1, (255 ,255,255))
-        text_hp = font1.render('Жизней: ' + str(hp), 1, (255 ,255,255))
-
-        window.blit(background,(0,0))
-
-        asteroids.draw(window)
-        monsters.draw(window)
-        bullets.draw(window)
-        
-        player.reset()
-
-        if death >= 10:
-            finish = True
-            window.blit(text_win,(225,250))      
-        if lost >= 10 or hp <= 0:
-            finish = True
-            window.blit(text_lose1,(225,250))
-
-        if rel_time == True:
-            now = timer()
-            if now - start < 3:
-               window.blit(text_reload,(310,410))
-            else:
-                num_fire = 0
-                rel_time = False
-        
-        window.blit(text_lose,(0,0))
-        window.blit(text_time,(0,30))
-        window.blit(text_hp,(0,60))
-        
-        display.update()
-    else:
-        finish = False
-        num_fire = 0
-        hp = 100
-        lost = 0
-        death = 0
-
-        for b in bullets:
-            b.kill()
-        for m in monsters:
-            m.kill()
-        for a in asteroids:
-            a.kill()
-        
-        time.delay(2000)
-        for _ in range(6):
-            monster = Enemy('ufo.png',randint(80,620), -40, 100, 60, randint(2,6))
-            monsters.add(monster)
-        for _ in range(3):
-            asteroid = Enemy('asteroid.png',randint(80,620), -40, 100, 60, randint(2,3))
-            asteroids.add(asteroid) 
-    
-    
-    time.delay(20)
+    display.update()
+    clock.tick(FPS)
